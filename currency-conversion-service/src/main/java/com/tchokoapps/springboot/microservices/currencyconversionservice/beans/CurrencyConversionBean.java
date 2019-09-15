834eefd69e43235fa800cf30015a5d257d24cda2
TchokoApps <tchokoapps@gmail.com>
2019-09-15 18:30:57 +0200

    update

--- currency-conversion-service/src/main/java/com/tchokoapps/springboot/microservices/currencyconversionservice/beans/CurrencyConversionBean.java
+++ currency-conversion-service/src/main/java/com/tchokoapps/springboot/microservices/currencyconversionservice/beans/CurrencyConversionBean.java
@@ -1,4 +1,30 @@
 package com.tchokoapps.springboot.microservices.currencyconversionservice.beans;
 
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.stereotype.Component;
+
+import java.math.BigDecimal;
+
+@Component
+@Data
+@NoArgsConstructor
 public class CurrencyConversionBean {
+    private Long id;
+    private String from;
+    private String to;
+    private BigDecimal conversionMultiple;
+    private BigDecimal quantity;
+    private BigDecimal totalCalculatedAmount;
+    private int port;
+
+    public CurrencyConversionBean(Long id, String from, String to, BigDecimal conversionMultiple, BigDecimal quantity, BigDecimal totalCalculatedAmount, int port) {
+        this.id = id;
+        this.from = from;
+        this.to = to;
+        this.conversionMultiple = conversionMultiple;
+        this.quantity = quantity;
+        this.totalCalculatedAmount = totalCalculatedAmount;
+        this.port = port;
+    }
 }
--- currency-conversion-service/src/main/java/com/tchokoapps/springboot/microservices/currencyconversionservice/controllers/CurrencyConversionController.java
+++ currency-conversion-service/src/main/java/com/tchokoapps/springboot/microservices/currencyconversionservice/controllers/CurrencyConversionController.java
@@ -1,4 +1,48 @@
 package com.tchokoapps.springboot.microservices.currencyconversionservice.controllers;
 
+import com.tchokoapps.springboot.microservices.currencyconversionservice.beans.CurrencyConversionBean;
+import com.tchokoapps.springboot.microservices.currencyconversionservice.proxies.CurrencyExchangeServiceProxy;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+import java.math.BigDecimal;
+import java.util.HashMap;
+import java.util.Map;
+
+@RestController
 public class CurrencyConversionController {
+
+    private CurrencyExchangeServiceProxy currencyExchangeServiceProxy;
+
+    public CurrencyConversionController(CurrencyExchangeServiceProxy currencyExchangeServiceProxy) {
+        this.currencyExchangeServiceProxy = currencyExchangeServiceProxy;
+    }
+
+    @GetMapping("/currency-converter/from/{from}/to/{to}/quantity/{quantity}")
+    public CurrencyConversionBean convertCurrency(@PathVariable String from, @PathVariable String to, @PathVariable BigDecimal quantity) {
+
+        Map<String, String> uriVariables = new HashMap<>();
+        uriVariables.put("from", from);
+        uriVariables.put("to", to);
+
+        ResponseEntity<CurrencyConversionBean> responseEntity = new RestTemplate().getForEntity("http://localhost:8000/currency-exchange/from/{from}/to/{to}",
+                CurrencyConversionBean.class, uriVariables);
+
+        CurrencyConversionBean responseBody = responseEntity.getBody();
+
+        return new CurrencyConversionBean(responseBody.getId(), responseBody.getFrom(), responseBody.getTo(),
+                responseBody.getConversionMultiple(), quantity, quantity.multiply(responseBody.getConversionMultiple()), responseBody.getPort());
+    }
+
+    @GetMapping("/currency-converter-feign/from/{from}/to/{to}/quantity/{quantity}")
+    public CurrencyConversionBean convertCurrencyFeign(@PathVariable String from, @PathVariable String to, @PathVariable BigDecimal quantity) {
+
+        CurrencyConversionBean responseBody = currencyExchangeServiceProxy.retrieveExchangeValue(from, to);
+
+        return new CurrencyConversionBean(responseBody.getId(), responseBody.getFrom(), responseBody.getTo(),
+                responseBody.getConversionMultiple(), quantity, quantity.multiply(responseBody.getConversionMultiple()), responseBody.getPort());
+    }
 }
