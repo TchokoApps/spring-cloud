956356a7dedf8c2dbacb35e5b62104471ef686fa
TchokoApps <tchokoapps@gmail.com>
2019-09-16 22:12:09 +0200

    upadate

---
+++ netflix-zuul-api-gateway-server/src/main/java/com/tchokoapps/springboot/microservices/netflixzuulapigatewayserver/ZuulLoggingFilter.java
@@ -0,0 +1,37 @@
+package com.tchokoapps.springboot.microservices.netflixzuulapigatewayserver;
+
+import com.netflix.zuul.ZuulFilter;
+import com.netflix.zuul.context.RequestContext;
+import com.netflix.zuul.exception.ZuulException;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.lang.builder.ToStringBuilder;
+import org.springframework.stereotype.Component;
+
+import javax.servlet.http.HttpServletRequest;
+
+@Slf4j
+@Component
+public class ZuulLoggingFilter extends ZuulFilter {
+    @Override
+    public String filterType() {
+        return "pre";
+    }
+
+    @Override
+    public int filterOrder() {
+        return 0;
+    }
+
+    @Override
+    public boolean shouldFilter() {
+        return true;
+    }
+
+    @Override
+    public Object run() throws ZuulException {
+        HttpServletRequest request = RequestContext.getCurrentContext().getRequest();
+        log.info(String.format("request -> %s, request uri -> %s", request, request.getRequestURI()));
+        log.info(String.format("request = %s", ToStringBuilder.reflectionToString(request)));
+        return request;
+    }
+}
